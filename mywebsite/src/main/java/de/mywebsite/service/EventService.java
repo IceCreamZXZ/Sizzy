06ff63ed00b844bb5e22ccba1e4ed7e72557458c
simon <simon@192.168.178.131>
2021-11-28 00:13:24 +0100

    Create EventEntity and EventService

---
+++ mywebsite/src/main/java/de/mywebsite/service/EventService.java
@@ -0,0 +1,66 @@
+package de.mywebsite.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import de.mywebsite.persistence.EventEntity;
+
+public class EventService {
+	private static EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+			.createEntityManagerFactory("mywebsite");
+	
+	public static void createEvent(String eventName, String host, String location, String game, int maxPlayer, int registeretPlayers) {
+		EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+		EntityTransaction et = null;
+		EventEntity event = new EventEntity();
+		try {
+			et = em.getTransaction();
+			et.begin();
+			event.setEventName(eventName);
+			event.setHost(host);
+			event.setLocation(location);
+			event.setGame(game);
+			event.setMaxPlayer(maxPlayer);
+			event.setRegisteretPlayers(registeretPlayers);
+			em.persist(event);
+			et.commit();
+		}
+		catch(Exception e) {
+			if(et != null) {
+				et.rollback();
+			}
+			e.printStackTrace();
+		}
+		finally {
+			em.close();
+		}
+		
+	}
+	
+	public static List<EventEntity> listAllEvents(int eventID) {
+		EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+		String query = "SELECT u FROM EventEntity u WHERE u.eventID = :eventID";
+		
+		TypedQuery<EventEntity> tq = em.createQuery(query, EventEntity.class);
+		List<EventEntity> result = new ArrayList<EventEntity>();
+		try {
+			result = tq.getResultList();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			em.close();
+		}
+		
+		return result;
+		
+	}
+	
+}
