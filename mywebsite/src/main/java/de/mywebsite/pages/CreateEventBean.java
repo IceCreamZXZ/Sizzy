d932f1d78c9a13ea02b0728ddf2d13894644e455
simon <simon@192.168.178.131>
2021-11-28 00:20:07 +0100

    created CreateEventBean

---
+++ mywebsite/src/main/java/de/mywebsite/pages/CreateEventBean.java
@@ -0,0 +1,74 @@
+package de.mywebsite.pages;
+
+import javax.faces.bean.ManagedBean;
+
+import de.mywebsite.service.EventService;
+
+@ManagedBean
+public class CreateEventBean {
+	private String eventName;
+	private String host;
+	private String location;
+	private String game;
+	private int maxPlayers;
+	private int registeretPlayers;
+	
+	public CreateEventBean() {
+		
+	}
+	
+	public String createEvent() {
+		EventService.createEvent(eventName, host, location, game, maxPlayers, registeretPlayers);
+		
+		return "welcome.xhtml";
+	}
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getGame() {
+		return game;
+	}
+
+	public void setGame(String game) {
+		this.game = game;
+	}
+
+	public int getMaxPlayers() {
+		return maxPlayers;
+	}
+
+	public void setMaxPlayers(int maxPlayers) {
+		this.maxPlayers = maxPlayers;
+	}
+
+	public int getRegisteretPlayers() {
+		return registeretPlayers;
+	}
+
+	public void setRegisteretPlayers(int registeretPlayers) {
+		this.registeretPlayers = registeretPlayers;
+	}
+	
+}
