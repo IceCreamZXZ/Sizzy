208b84817b40179f85bf0e39ebef324b452565e0
simon <simon@mbp-von-simon.fritz.box>
2021-12-08 18:09:44 +0100

    design zeug

--- mywebsite/src/main/java/de/mywebsite/pages/WelcomeBean.java
+++ mywebsite/src/main/java/de/mywebsite/pages/WelcomeBean.java
@@ -1,8 +1,22 @@
 package de.mywebsite.pages;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import de.mywebsite.model.UserModel;
 
 @ManagedBean
 public class WelcomeBean {
+	
+	@ManagedProperty("#{userModel}")
+	private UserModel userModel;
+
+	public UserModel getUserModel() {
+		return userModel;
+	}
+
+	public void setUserModel(UserModel userModel) {
+		this.userModel = userModel;
+	}
 	
 }
