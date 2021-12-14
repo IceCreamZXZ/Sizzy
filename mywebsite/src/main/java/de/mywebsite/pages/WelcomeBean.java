25470ae643986d4767f34cec660213f312e3ba5b
simon <simon@simon-budzus-3c06303a282e.fds-limburg.schule>
2021-12-14 10:13:32 +0100

    shit

--- mywebsite/src/main/java/de/mywebsite/pages/WelcomeBean.java
+++ mywebsite/src/main/java/de/mywebsite/pages/WelcomeBean.java
@@ -1,32 +1,59 @@
 package de.mywebsite.pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 
+import de.mywebsite.model.EventModel;
 import de.mywebsite.model.UserModel;
+import de.mywebsite.service.EventService;
 
 @ManagedBean
+@RequestScoped
 public class WelcomeBean {
-	
 	@ManagedProperty("#{userModel}")
-	private UserModel userModel;
+	private UserModel user;
+	
+	@ManagedProperty("#{eventModel}")
+	private EventModel eventModel;
+	
+	List<EventModel> list = new ArrayList<EventModel>();
 
 	public WelcomeBean() {
-		
+		list = EventService.eventsForUser(getUser().getUsername());
 	}
 	
 	public String signOut() {
-		userModel.flush();
+		user.flush();
 		
 		return "index.xhtml";
 	}
-	
-	public UserModel getUserModel() {
-		return userModel;
+
+	public EventModel getEventModel() {
+		return eventModel;
 	}
 
-	public void setUserModel(UserModel userModel) {
-		this.userModel = userModel;
+	public void setEventModel(EventModel eventModel) {
+		this.eventModel = eventModel;
+	}
+
+	public List<EventModel> getList() {
+		return list;
+	}
+
+	public void setList(List<EventModel> list) {
+		this.list = list;
+	}
+
+	public UserModel getUser() {
+		return user;
+	}
+
+	public void setUser(UserModel user) {
+		this.user = user;
 	}
 	
 }
