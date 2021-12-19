82e3e788f8948110a49a215d72526a256a9084eb
simon <simon@mbp-von-simon.fritz.box>
2021-12-19 15:57:18 +0100

    Drop down Menu design

--- mywebsite/src/main/java/de/mywebsite/pages/WelcomeBean.java
+++ mywebsite/src/main/java/de/mywebsite/pages/WelcomeBean.java
@@ -8,11 +8,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
-import org.primefaces.model.menu.DefaultMenuItem;
-import org.primefaces.model.menu.DefaultMenuModel;
-import org.primefaces.model.menu.DefaultSubMenu;
-import org.primefaces.model.menu.MenuModel;
-
 import de.mywebsite.model.EventModel;
 import de.mywebsite.model.UserModel;
 import de.mywebsite.service.EventService;
@@ -29,8 +24,6 @@ public class WelcomeBean {
 	
 	
 	List<EventModel> list = new ArrayList<EventModel>();
-	
-	private MenuModel model;
 	
 	public WelcomeBean() {
 		
@@ -92,14 +85,6 @@ public class WelcomeBean {
 
 	public void setList(List<EventModel> list) {
 		this.list = list;
-	}
-
-	public MenuModel getModel() {
-		return model;
-	}
-
-	public void setModel(MenuModel model) {
-		this.model = model;
 	}
 	
 }
